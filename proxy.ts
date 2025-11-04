947472be0f7a2b66fde470334b14cfd6c0dac4e2
PranavAvasthi <pranavavasthi44@gmail.com>
2025-11-04 18:30:06 +0530

    migrated from middleware to proxy

---
+++ proxy.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function proxy(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const supportedPaths = ["/profile"];
+
+  const shouldHandle = supportedPaths.some((path) => pathname.startsWith(path));
+  if (!shouldHandle) return NextResponse.next();
+
+  const appScheme = `https://deeplink-app-blush.vercel.app${pathname}${req.nextUrl.search}`;
+
+  console.log(appScheme);
+
+  const fallbackUrl =
+    "https://play.google.com/store/apps/details?id=com.instagram.android";
+
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script>
+          window.location.href = "${appScheme}";
+          
+          setTimeout(() => {
+            window.location.replace("${fallbackUrl}");
+          }, 1000);
+        </script>
+      </head>
+      <body></body>
+    </html>
+  `;
+
+  return new NextResponse(html, {
+    headers: { "Content-Type": "text/html" },
+  });
+}
+
+export const config = {
+  matcher: ["/profile/:path*"],
+};
