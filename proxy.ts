339bcb1c7d86e7d130d5d8b82b8f883f1ea4c05c
PranavAvasthi <pranavavasthi44@gmail.com>
2025-11-05 13:08:27 +0530

    revert proxy changes

--- proxy.ts
+++ proxy.ts
@@ -1,40 +1,3 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export function proxy(req: NextRequest) {
-//   const pathname = req.nextUrl.pathname;
-//   const supportedPaths = ["/profile"];
-
-//   const shouldHandle = supportedPaths.some((path) => pathname.startsWith(path));
-//   if (!shouldHandle) return NextResponse.next();
-
-//   const appBase = "deeplink-app-blush.vercel.app";
-//   const fullPath = `${appBase}${pathname}${req.nextUrl.search}`;
-
-//   const intentLink = `intent://${fullPath}#Intent;scheme=https;package=com.instagram.android;end`;
-
-//   const html = `
-//     <!DOCTYPE html>
-//     <html>
-//       <head>
-//         <meta name="viewport" content="width=device-width, initial-scale=1">
-//         <script>
-//           window.location.href = "${intentLink}";
-//         </script>
-//       </head>
-//       <body></body>
-//     </html>
-//   `;
-
-//   return new NextResponse(html, {
-//     headers: { "Content-Type": "text/html" },
-//   });
-// }
-
-// export const config = {
-//   matcher: ["/profile/:path*"],
-// };
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -48,9 +11,7 @@ export function proxy(req: NextRequest) {
   const appBase = "deeplink-app-blush.vercel.app";
   const fullPath = `${appBase}${pathname}${req.nextUrl.search}`;
 
-  const packageName = "com.instagram.android";
-  const playStoreUrl = `https://play.google.com/store/apps/details?id=${packageName}`;
-  const intentLink = `intent://${fullPath}#Intent;scheme=https;package=${packageName};end`;
+  const intentLink = `intent://${fullPath}#Intent;scheme=https;package=com.instagram.android;end`;
 
   const html = `
     <!DOCTYPE html>
@@ -58,17 +19,10 @@ export function proxy(req: NextRequest) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script>
-
-          window.location.replace("${intentLink}");
-          
-          setTimeout(() => {
-            window.location.replace("${playStoreUrl}");
-          }, 2000);
+          window.location.href = "${intentLink}";
         </script>
       </head>
-      <body>
-        <p>Redirecting...</p>
-      </body>
+      <body></body>
     </html>
   `;
 
